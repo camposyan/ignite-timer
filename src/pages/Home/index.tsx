9972493fa81714c047ce60ae1bc51c9fae31cc99
Yan Campos <camposyan00@gmail.com>
2022-11-04 15:04:22 -0300

    feat: desabilitando botão quando inputs estão vazios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,11 +3,14 @@ import { useForm } from "react-hook-form";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 export function Home() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, watch } = useForm();
 
-  function handleCreateNewCycle(data:any) {
+  function handleCreateNewCycle(data: any) {
     console.log(data)
   }
+
+  const task = watch('task');
+  const isSubmitDisabled = !task;
 
   return (
     <HomeContainer>
@@ -19,7 +22,7 @@ export function Home() {
             type="text"
             placeholder="Dê um nome para o seu projeto"
             list="task-suggestions"
-            {...register('task', { valueAsNumber: true })}
+            {...register('task')}
             />
 
           <datalist id="task-suggestions">
@@ -37,7 +40,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
-            {...register('minutesAmount')}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -51,7 +54,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
